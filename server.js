a6763b454b85f7da966b90da89821a396df1459f
crsviray <viraycrisit1a@gmail.com>
2017-03-21 23:37:10 +0800

    second

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(('/js'),express.static(path.join(__dirname, 'assets/js')));
+app.use(('/img'),express.static(path.join(__dirname, 'assets/images')));
+app.use(('/css'),express.static(path.join(__dirname, 'assets/css')));
+app.use(('/fonts'),express.static(path.join(__dirname, 'assets/fonts')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/app.html'));
+  res.sendFile(path.join(__dirname, 'assets/index.html'));
 });
 app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/about.html'));
+  res.sendFile(path.join(__dirname, 'assets/generic.html'));
 });
 app.get('*', function(req, res){
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
