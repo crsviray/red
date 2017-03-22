daa84289a02b864d03fd7efe9f30302dd9e82978
crsviray <viraycrisit1a@gmail.com>
2017-03-22 09:38:25 +0800

    dv.1

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//server.js
-//require modules
 var express = require('express');
 var path = require('path');
 //instantiate express
@@ -7,17 +5,13 @@ var app = express();
 //set port
 app.set('port', (process.env.PORT || 5000));
 //use static files
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(('/js'),express.static(path.join(__dirname, '/assets/js')));
-app.use(('/img'),express.static(path.join(__dirname, '/assets/images')));
-app.use(('/css'),express.static(path.join(__dirname, '/assets/css')));
-app.use(('/fonts'),express.static(path.join(__dirname, '/assets/fonts')));
+app.use(express.static(path.join(__dirname, 'assets')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/assets/index.html'));
+  res.sendFile(path.join(__dirname, 'assets/index.html'));
 });
-app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname, '/assets/generic.html'));
+app.get('/generic', function(req, res){
+res.sendFile(path.join(__dirname, 'assets/generic.html'));
 });
 //express server listen
 var server = app.listen(app.get('port'), function(){
